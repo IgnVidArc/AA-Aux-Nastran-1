08e5103a9ecf032cdde9e0fb8f9952b7d3f072de
Ignacio Vidal de Arce <ignacio.vidal.arce@gmail.com>
2024-03-09 20:36:27 +0100

    save fig task3 section4

--- restults_Task3_Coupling.m
+++ restults_Task3_Coupling.m
@@ -1,32 +1,32 @@
 %% TASK 3: EVOLUTION OF NATURAL FREQUENCIES WITH kCOUPLING:
-
-1.191159E+01
-2.140359E+01
-
-%%
 k1 = 3947.84;
 a = [0, .5, 1, 1.5, 2.5, 5, 50, 1000]';
 k = a*k1;
 
-% File:
-filename = 'Task2_SOL112.pch';
-% Line of first data entry to retrieve:
-StartLine = 8;
-% Number of time steps to read:
-NtimeSteps = 1000;
+freqs = [
+9.999998E+00, 2.000000E+01;
+1.191159E+01, 2.140359E+01;
+1.302775E+01, 2.302775E+01;
+1.370649E+01, 2.474130E+01;
+1.443788E+01, 2.813445E+01;
+1.509916E+01, 3.566532E+01;
+1.574009E+01, 1.012534E+02;
+1.580783E+01, 4.474930E+02
+]
 
-%% Reading the displacements of 1001:
-[times, states] = ReadTranscientResponse(filename, StartLine, NtimeSteps);
+figure(1)
+loglog(a, freqs(:,:), '-s')
+% set(gca, 'YScale', 'log')
+% set(gca, 'XScale', 'log')
+xlim([.5,1000])
+legend({'f1', 'f2'})
 
-%% Reading the loads on 1001:
-StartLine = 8052;
-[~, loads] = ReadTranscientResponse(filename, StartLine, NtimeSteps);
 
 %%
-figure
-plot(times, states(:,3))
-figure
-plot(times, loads(:,3))
+k1 = 3947.84;
+k2 = 15791.36;
+M1 = 1;
+cycles_k8_eig1 = 1/(2*pi) * sqrt((k1+k2)/2*M1)
 
 
 
