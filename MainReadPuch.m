dd5fc870d4accc07573f739e937aa4c2ec3675a7
Ignacio Vidal de Arce <ignacio.vidal.arce@gmail.com>
2024-03-09 18:34:52 +0100

    Update MainReadPuch.m

--- MainReadPuch.m
+++ MainReadPuch.m
@@ -1,6 +1,6 @@
 %% READNG A .pch FILE TO PLOT THE RESPONSE
 % File:
-filename = 'testpch.pch';
+filename = 'Task2_SOL112.pch';
 % Line of first data entry to retrieve:
 StartLine = 8;
 % Number of time steps to read:
