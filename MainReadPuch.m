50d7bdbd7029358c90243ff4af4f0bf3c2cdb92d
Ignacio Vidal de Arce <ignacio.vidal.arce@gmail.com>
2024-03-09 18:33:47 +0100

    función hecha

---
+++ MainReadPuch.m
@@ -0,0 +1,19 @@
+%% READNG A .pch FILE TO PLOT THE RESPONSE
+% File:
+filename = 'testpch.pch';
+% Line of first data entry to retrieve:
+StartLine = 8;
+% Number of time steps to read:
+NtimeSteps = 1000;
+
+[times, states] = ReadTranscientResponse(filename, StartLine, NtimeSteps);
+
+%%
+figure
+plot(times, states(:,3))
+
+
+
+
+
+
---
+++ ReadTranscientResponse.m
@@ -0,0 +1,37 @@
+function [TIMES, STATES] = ReadTranscientResponse(filename, StartLine, NtimeSteps)
+
+    fid = fopen(filename, 'r');
+
+    TIMES = zeros(NtimeSteps,1);
+    STATES = zeros(NtimeSteps,6);
+    
+    % Skip some lines:
+    for i = 1:StartLine - 1
+        fgetl(fid);
+    end
+    
+    for k=1:NtimeSteps+1+1
+        disp(k)
+        % Normal line:
+        data_cell = textscan(fid, '%s', 6);
+    
+        t = str2double(cell2mat(data_cell{1,1}(1)));
+        x = str2double(cell2mat(data_cell{1,1}(3)));
+        y = str2double(cell2mat(data_cell{1,1}(4)));
+        z = str2double(cell2mat(data_cell{1,1}(5)));
+        
+        % Cont line:
+        data_cell = textscan(fid, '%s', 5);
+    
+        turnx = str2double(cell2mat(data_cell{1,1}(2)));
+        turny = str2double(cell2mat(data_cell{1,1}(3)));
+        turnz = str2double(cell2mat(data_cell{1,1}(4)));
+    
+        % Storing the data:
+        TIMES(k) = t;
+        STATES(k,:) = [x,y,z,turnx,turny,turnz];
+    
+    end
+    
+    fclose(fid);
+end
